499ac6494e581f0a0e0026671cee1c4b31a90a27
Senior-Uz <omadbekclever2@gmail.com>
2022-06-13 11:48:50 +0500

    addded git ignore and some bug fixes

--- app.js
+++ app.js
@@ -133,9 +133,13 @@ app.get("/", function(req, res) {
   })
   
 
-
+  let port = process.env.PORT;
+  if (port == null || port == "") {
+    port = 3000;
+  }
+  app.listen(port);
   
-  app.listen(3000, function() {
-    console.log("Server started on port 3000");
+  app.listen(port, function() {
+    console.log("Server started successfully");
   });
   
